2b9bc2a6cbf3cfc83d776bc899602d4c10e8183b
root <saifraza308@gmail.com>
2018-01-27 23:20:52 +0500

    final approach

--- hub.js
+++ hub.js
@@ -1,3 +1,4 @@
+$("#disappear").hide();
 function firebasesave(){
   var email=document.getElementById("email").value;
   var password=document.getElementById("pwd").value;
@@ -11,56 +12,37 @@ else {
     console.log(success);
   }).catch(function(error){
     varx=document.getElementById("email_msg").innerHTML="<div class='alert alert-danger' style='margin:0 auto;text-align:center'>"+error.message+"</div>";
-
-    console.log(error);
+console.log(error);
   })
-
-}
-}
+}}
 function addtodo(){
   var firebaseref=firebase.database().ref();
-
   var todo=document.getElementById("todo").value;
-
   firebaseref.child("Todo").push().child("Todo").set(todo).then(function(response){
-
   }).catch(function(error){
-    //console.log(error);
-  });
+//console.log(error);
+});
 
 }
 var firebaseref=firebase.database().ref('Todo');
 firebaseref.on("value",getResponse,getError);
 function getResponse(response){
   var description=document.getElementById('description');
-
-
-
   description.innerHTML="";
   var objects=response.val();
   var todos=Object.keys(objects);
-
-
-
   for(var i=0 ; i< todos.length ; i++)
   {
     var t=todos[i];
-
-
     var todo=objects[t].Todo;
-
     description.innerHTML+="<td >Todo :"+i+"</td><td >"+todo+"</td><td><a class='btn btn-danger' id='delbtn'  onclick='deltodo("+i+")'>Delete</a></td><td ><a class='btn btn-primary' id='edit' onclick='edittodo("+i+")'>Edit</a></td>";
-
-
   }
   }
-
 function getError(error){
   console.log(error);
 }
 function deltodo(i)
 {
-
   var delRef=firebase.database().ref("Todo");
   delRef.once("value",function(response){
     var objects=response.val();
@@ -68,50 +50,38 @@ function deltodo(i)
       var key=todos[i];
       firebase.database().ref("Todo").child(key).remove();
       alert("Successfully Deleted The Record");
-
-  })
-  }
+})
+}
   function edittodo(j)
   {
-
-    var delRef=firebase.database().ref("Todo");
+    $("#disappear").show();
+  var delRef=firebase.database().ref("Todo");
   delRef.once("value",function(response){
   var objects=response.val();
   var edit=objects.Todo;
-
   var todos=Object.keys(objects);
-
   for(var i=0 ; i< todos.length ; i++)
   {
     var t=todos[i];
-
     var value=i;
     var todo=objects[t].Todo;
-
     var object=[];
     object.push(i,todo,todos[j]);
     if(object[0]===j)
     {
-
       var edit=object[1];
-
       var key=object[2];
       var edittodo=document.getElementById("edit_todo_data");
       var keytodo=document.getElementById("edit_todo_key");
       var update=document.getElementById("update-btn");
       edittodo.value=edit;
       keytodo.value=key;
-      update.innerHTML="<center><button class='btn btn-primary' onclick='updatetodo()' >Update Todo</button></center>"
-
-     }
 }
-})
-
-}
+}})}
 function updatetodo()
 {
-  var updatetodo=document.getElementById("edit_todo_data").value;
-  var updatekey=document.getElementById("edit_todo_key").value;
+    var updatetodo=document.getElementById("edit_todo_data").value;
+    var updatekey=document.getElementById("edit_todo_key").value;
     console.log(updatekey);
     console.log(updatetodo);
     var data={
@@ -120,8 +90,7 @@ function updatetodo()
     var updates={};
     updates[updatekey]=data;
     firebase.database().ref("Todo").update(updates);
-    
+    alert("Successfully updated the Record");
+    $("#disappear").hide();
 
-
-
-}
+    }
